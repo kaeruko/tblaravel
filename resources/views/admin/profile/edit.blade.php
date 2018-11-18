458fb884677c9ea23ad18b99fc9ab86422b1c954
kaeruko <mail@cloxs.jp>
2018-11-18 17:31:44 +0900

    PHP/Laravel 16 challenge

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -14,7 +14,7 @@
                             @endforeach
                         </ul>
                     @endif
-                    <div class="form-group row">
+                        <div class="form-group row">
                         <label class="col-md-2" for="name">氏名</label>
                         <div class="col-md-10">
                             <input type="text" class="form-control" name="name" value="{{ old('title') }}">
