ad4947062c33a6be6cc2c9c4ade6de588940d931
kaeruko <mail@cloxs.jp>
2018-11-18 14:33:29 +0900

    PHP/Laravel 14 challenge

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::group(['prefix' => 'admin'], function() {
 	Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
 	Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');
     Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
+    Route::post('profile/edit', 'Admin\ProfileController@update')->middleware('auth');
 });
 
 Auth::routes();
