908864ad023a735fec72430d0dcc4fff73e6be0a
kaeruko <mail@cloxs.jp>
2018-11-18 11:38:59 +0900

    PHP/Laravel 12 challenge

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,6 @@ Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
     Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
