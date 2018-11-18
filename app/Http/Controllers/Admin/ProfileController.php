7a9666d8896fc7820875410db7dee808c3476a76
kaeruko <mail@cloxs.jp>
2018-11-18 18:56:46 +0900

    PHP/Laravel 17 challenge

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+
+use App\Profile;
+
 class ProfileController extends Controller
 {
 	public function edit()
@@ -12,8 +15,22 @@ class ProfileController extends Controller
 		return view('admin.profile.edit');
 	}
 
-	public function update()
+	public function update(Request $request)
 	{
-		return redirect('admin/profile/edit');
+      // Varidationを行う
+      $this->validate($request, Profile::$rules);
+
+      // 既存のプロフィールデータを削除
+      Profile::truncate();
+
+      $profile = new Profile;
+      $form = $request->all();
+
+      unset($form['_token']);
+
+      $profile->fill($form);
+      $profile->save();
+
+      return redirect('admin/profile/edit');
 	}
 }
--- database/migrations/2018_11_18_095458_create_profile_table.php
+++ database/migrations/2018_11_18_095458_create_profile_table.php
@@ -13,8 +13,7 @@ class CreateProfileTable extends Migration
      */
     public function up()
     {
-
-        Schema::create('profile', function (Blueprint $table) {
+        Schema::create('profiles', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->integer('gender')->nullable();
@@ -31,6 +30,6 @@ class CreateProfileTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('profile');
+        Schema::dropIfExists('profiles');
     }
 }
