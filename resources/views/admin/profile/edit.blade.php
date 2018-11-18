11193ff5b7216256ece7d791e164d4763dbe5837
kaeruko <mail@cloxs.jp>
2018-11-18 14:01:31 +0900

    PHP/Laravel 13 challenge

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -3,14 +3,14 @@
 
 
 {{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
-@section('title', 'ニュースの編集')
+@section('title', 'プロフィールの編集')
 
 {{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>ニュース編集</h2>
+                <h2>プロフィール編集</h2>
             </div>
         </div>
     </div>
